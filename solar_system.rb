0429c29da478276098ecbee8a510b5cf1926400f
Chantal Demissie <42252976+ChantalDemissie@users.noreply.github.com>
2019-02-26 21:45:04 -0800

    Add files via upload

---
+++ solar_system.rb
@@ -0,0 +1,29 @@
+class SolarSystem
+  def initialize(star_name)
+    @star_name = star_name
+    @planets = []
+  end
+
+  def star_name
+    return @star_name
+  end
+
+  def planets
+    return @planets
+  end
+
+  def add_planet(planet)
+    @planets.push(planet)
+  end
+
+  def list_planets
+    str = "Planets orbiting #{@star_name}\n"
+    @planets.each_with_index do |planet, index|
+      str += "#{index + 1}.  #{planet.name}\n"
+    end
+    return str
+  end
+
+  def SolarSystem_find_planet_by_name
+  end
+end
